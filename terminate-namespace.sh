e1766ec174a3c6372b37f8957264e252f097e572
Rhonda  Toh <rhonda.toh.e@thalesdigital.io>
2023-05-22 15:18:04 +0800

    chore: modify script for terminating namespaces

--- terminate-namespace.sh
+++ terminate-namespace.sh
@@ -8,12 +8,17 @@ wait_time=3
 if [ -z "${terminating_namespaces[@]}" ]; then
     echo "No terminating namespaces detected" 
     exit 0
+else 
+    echo "Terminating namespaces detected: " 
+    for namespace in $terminating_namespaces; do
+        echo $namespace
+    done 
 fi
 
 # Loop through the terminating namespaces
 for namespace in $terminating_namespaces; do
     if [[ $namespace == $namespace_prefix* ]]; then
-        echo "Terminating namespace $namespace detected. Beginning cleanup" 
+        echo "Beginning cleanup for $namespace" 
         flink_deployments=$(kubectl -n $namespace get flinkdeployments.flink.apache.org -o json | jq -r '.items[] | .metadata.name')
         for deployment in $flink_deployments; do
             echo "patching the finalizer for cr flinkdeployments.flink.apache.org $deployment"
@@ -25,12 +30,13 @@ done
 echo "Deployments patched. Waiting $wait_time seconds to confirm that the namespaces are terminated."
 sleep $wait_time
 
-if [ -z "${terminating_namespaces[@]}" ]; then
+still_terminating_namespaces=$(kubectl get namespaces --output=json | jq -r '.items[] | select(.status.phase=="Terminating") | .metadata.name')
+if [ -z "${still_terminating_namespaces[@]}" ]; then
     echo "All terminating namespace have been terminated." 
     exit 0
 else 
     echo "The following namespaces failed to terminate." 
-    for namespace in $terminating_namespaces; do
+    for namespace in $still_terminating_namespaces; do
         echo $namespace
     done 
 fi
