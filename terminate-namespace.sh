2f6415ffe215613c88570c1d7f02618926d46a2e
rhonda.toh <rhonda.toh.e@thalesdigital.io>
2023-12-12 12:36:19 +0800

    chore: update terminate namespace script to also handle old namespaces

--- terminate-namespace.sh
+++ terminate-namespace.sh
@@ -1,44 +1,71 @@
-#!/bin/bash
+#!/bin/bash 
 
-# Get all namespaces and filter the terminating ones
-terminating_namespaces=$(kubectl get namespaces --output=json | jq -r '.items[] | select(.status.phase=="Terminating") | .metadata.name')
+namespace_regex="^session-[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$"
 namespace_prefix="session"
 wait_time=10
+aging_time=30
 
-if [ -z "${terminating_namespaces[@]}" ]; then
-    echo "No terminating namespaces detected" 
-    exit 0
-else 
-    echo "Terminating namespaces detected: " 
-    for namespace in $terminating_namespaces; do
-        echo $namespace
+date=$(date -u -v-"$aging_time"d '+%Y-%m-%dT%H:%M:%SZ')
+
+function remove_flink_deployments {
+    local namespace="$1"
+    echo "Beginning cleanup for $namespace" 
+    flink_deployments=$(kubectl -n $namespace get flinkdeployments.flink.apache.org -o json | jq -r '.items[] | .metadata.name')
+    for deployment in $flink_deployments; do
+        echo "patching the finalizer for cr flinkdeployments.flink.apache.org $deployment"
+        kubectl -n $namespace patch flinkdeployments.flink.apache.org/$deployment -p '{"metadata":{"finalizers":[]}}' --type=merge
     done
-    echo "" 
-fi
+    echo ""
+}
 
-# Loop through the terminating namespaces
-for namespace in $terminating_namespaces; do
-    if [[ $namespace == $namespace_prefix* ]]; then
-        echo "Beginning cleanup for $namespace" 
-        flink_deployments=$(kubectl -n $namespace get flinkdeployments.flink.apache.org -o json | jq -r '.items[] | .metadata.name')
-        for deployment in $flink_deployments; do
-            echo "patching the finalizer for cr flinkdeployments.flink.apache.org $deployment"
-            kubectl -n $namespace patch flinkdeployments.flink.apache.org/$deployment -p '{"metadata":{"finalizers":[]}}' --type=merge
+function remove_aging_namespaces {
+    old_namespaces=$(kubectl get namespaces --output=json | jq -r --arg date "$date" '.items[] | select(.metadata.creationTimestamp < $date) | .metadata.name')
+
+    for namespace in $old_namespaces; do 
+        if [[ $namespace =~ $namespace_regex ]]; then 
+            echo "deleting namespace $namespace"
+            kubectl delete namespace $namespace
+        fi
+    done
+}
+
+function remove_terminating_namespaces {
+    # Get all namespaces and filter the terminating ones
+    terminating_namespaces=$(kubectl get namespaces --output=json | jq -r '.items[] | select(.status.phase=="Terminating") | .metadata.name')
+    
+
+    if [ -z "${terminating_namespaces[@]}" ]; then
+        echo "No terminating namespaces detected" 
+        return 1
+    else 
+        echo "Terminating namespaces detected: " 
+        for namespace in $terminating_namespaces; do
+            echo $namespace
         done
-        echo ""
-    fi 
-done
+        echo "" 
+    fi
 
-echo "Deployments patched. Waiting $wait_time seconds to confirm that the namespaces are terminated."
-sleep $wait_time
+    # Loop through the terminating namespaces
+    for namespace in $terminating_namespaces; do
+        if [[ $namespace == $namespace_prefix* ]]; then
+            remove_flink_deployments $namespace
+        fi 
+    done
 
-still_terminating_namespaces=$(kubectl get namespaces --output=json | jq -r '.items[] | select(.status.phase=="Terminating") | .metadata.name')
-if [ -z "${still_terminating_namespaces[@]}" ]; then
-    echo "All terminating namespace have been terminated." 
-    exit 0
-else 
-    echo "The following namespaces failed to terminate." 
-    for namespace in $still_terminating_namespaces; do
-        echo $namespace
-    done 
-fi
+    echo "Deployments patched. Waiting $wait_time seconds to confirm that the namespaces are terminated."
+    sleep $wait_time
+
+    still_terminating_namespaces=$(kubectl get namespaces --output=json | jq -r '.items[] | select(.status.phase=="Terminating") | .metadata.name')
+    if [ -z "${still_terminating_namespaces[@]}" ]; then
+        echo "All terminating namespace have been terminated." 
+        return 0
+    else 
+        echo "The following namespaces failed to terminate." 
+        for namespace in $still_terminating_namespaces; do
+            echo $namespace
+        done 
+    fi
+}
+
+# remove_aging_namespaces
+remove_terminating_namespaces
